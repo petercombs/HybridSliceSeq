d7d4a91ca2618f70da60c2fd27cc1c340fca4b84
petercombs <pcombs@stanford.edu>
2015-08-06 21:36:47 -0700

    Fasta Splitter that uppercases everything

---
+++ faSplitter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from Bio import SeqIO
+from sys import argv, exit
+from os import path, makedirs
+
+if len(argv) != 3:
+    print("""Usage: {} FILENAME OUTDIR
+
+FILENAME:       A FASTA file containing the sequences to split
+OUTDIR:         The directory name to output split sequences to (will create if necessary)
+""".format(argv[0]))
+    exit(1)
+
+if not path.exists(argv[2]):
+    mkdirs(argv[2])
+
+if not path.isdir(argv[2]):
+    print("{} is not a directory".format(argv[2]))
+    exit(2) 
+
+for rec in SeqIO.parse(argv[1], 'fasta'):
+    rec.seq = rec.seq.upper()
+    SeqIO.write(rec, path.join(argv[2], rec.id + '.fa'), 'fasta')
+
