83ca3bb362cedbb3d17b77ba8e1b52bce7155db6
petercombs <pcombs@stanford.edu>
2015-10-28 16:25:45 -0700

    Fix bug in GetTrueHets
    I previously thought that the bases needed to be complemented; this is
    not actually true.

--- GetTrueHets.py
+++ GetTrueHets.py
@@ -6,15 +6,17 @@ from collections import defaultdict, Counter
 if __name__ == "__main__":
     snps_all = defaultdict(Counter)
     for fname in argv[1:]:
+        print("Reading", fname)
         snps_i = pd.read_table(fname)
         for i, row in snps_i.iterrows():
             snps_current = snps_all[(row.CHR, row.POSITION)]
             for base, count in zip('ACGT', row['POS_A|C|G|T'].split('|')):
                 snps_current[base] += int(count)
             # Note that the bases have been complemented here
-            for base, count in zip('TGCA', row['NEG_A|C|G|T'].split('|')):
+            for base, count in zip('ACGT', row['NEG_A|C|G|T'].split('|')):
                 snps_current[base] += int(count)
 
+    print("Writing")
     with open('true_hets.tsv', 'w') as out:
         for key, counts in snps_all.items():
             _, c2 = counts.most_common(2)[1]
