5f4711c58b417f85d3b5befcb6d41a0b2d132e8d
petercombs <pcombs@stanford.edu>
2016-03-04 16:39:46 -0800

    Code for creating mel and sim specific fasta files

---
+++ MakeSimVersion.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from Bio import SeqIO, Seq
+from os import path
+
+def replace_variant(variant, gt, sequence):
+    var = variant[gt].split('/')[0]
+    pos = variant.POS - 1
+    if var == '*':
+        sequence.pop(pos)
+        return -len(variant.REF)
+    elif var == '.' or (sequence[pos:pos + len(variant.REF)]
+                        == var):
+        return 0
+    elif len(var) == len(variant.REF):
+        sequence[pos:pos+len(var)] = var
+        return 0
+    else:
+        print(sequence[pos:pos+len(variant.REF)])
+        for i, c in enumerate(variant.REF):
+            sequence.pop(pos)
+        for c in reversed(var):
+            sequence.insert(pos, c)
+        return len(variant.REF) - len(var)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    mel_genome = {
+        rec.id: rec.seq
+        for rec in SeqIO.parse('prereqs/dmel-all-chromosome-r6.06.fasta', 'fasta')
+    }
+
+
+    regions = pd.read_table('prereqs/dnase_acc_spatial.tsv').ix[:, 1]
+    vars = pd.read_table('analysis/on_mel/melsim_variants.tsv')
+
+    for region in regions:
+        chrom = region.split(':')[0]
+        mel_chr = Seq.MutableSeq(str(mel_genome[chrom]))
+        sim_chr = Seq.MutableSeq(str(mel_genome[chrom]))
+        coords = region.split(':')[1].split('..')
+        start = int(coords[0])
+        stop = int(coords[1])
+        mel_diff = 0
+        sim_diff = 0
+        region_vars = vars.query('CHROM == "dmel_{}" and {} < POS and POS <= {}'
+                                 .format(chrom, start, stop))
+        lowest = 1e10
+        for var_ix in reversed(region_vars.index):
+            var = region_vars.ix[var_ix]
+            assert lowest > var.POS
+            lowest = var.POS
+            mel_diff += replace_variant(var, 'mel_gdna.GT', mel_chr)
+            sim_diff += replace_variant(var, 'sim_gdna.GT', sim_chr)
+
+        SeqIO.write([SeqIO.SeqRecord(mel_chr[start:stop+mel_diff], 'mel_'+region),
+                     SeqIO.SeqRecord(sim_chr[start:stop+sim_diff], 'sim_'+region)],
+                    path.join('analysis', 'results', region.replace(':','_')+'.fasta'),
+                    'fasta')
+
+
+
+
