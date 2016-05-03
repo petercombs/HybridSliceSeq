e81fc462256abf537d70600a46346be991bafaa3
petercombs <pcombs@stanford.edu>
2016-05-02 17:28:30 -0700

    Attempt to diagnose why parentals have reads that don't match the SNPs

--- CheckSNPs.py
+++ CheckSNPs.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sys import argv
 from os import path
 import numpy as np
+import editdistance as ed
 from collections import defaultdict
 
 isfinite = lambda x: x is not np.nan
@@ -31,14 +32,22 @@ def get_linked_snps(snps_matching, n_linked=2):
     num_snps = snps_matching.applymap(isfinite).sum(axis=1)
     return snps_matching.ix[num_snps >= n_linked].dropna(how='all', axis=1)
 
-def class_refalt(snps_in_read, ref_alt_dict):
+def class_refalt(snps_in_read, ref_alt_dict, seen_snps = set()):
     snps_in_read = snps_in_read.dropna()
     snp_pos = snps_in_read.index
     snps_in_read = ''.join(snps_in_read)
-    if snps_in_read == ''.join(ref_alt_dict[i-1][1] for i in snp_pos):
+    altseq = ''.join(ref_alt_dict[i-1][1] for i in snp_pos)
+    if (snps_in_read == altseq) or (snps_in_read == altseq.translate(complement)):
         return 1
-    elif snps_in_read == ''.join(ref_alt_dict[i-1][0] for i in snp_pos):
+    refseq = ''.join(ref_alt_dict[i-1][0] for i in snp_pos)
+    if (snps_in_read == refseq) or (snps_in_read == refseq.translate(complement)):
         return -1
+    if len(snps_in_read) < 3:
+        pass
+    elif seen_snps is not None and snps_in_read not in seen_snps:
+        diff_ed = ed.eval(snps_in_read, refseq) - ed.eval(snps_in_read, altseq)
+        print(snps_in_read, refseq, altseq, ['==', '=>', '<='][np.sign(diff_ed)], sep='\t')
+        seen_snps.add(snps_in_read)
     return 0
     
 
