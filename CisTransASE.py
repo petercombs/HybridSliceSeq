9eec3cddc7b99932a87d43ad6dbae26223278ea5
petercombs <pcombs@stanford.edu>
2016-07-05 10:38:19 -0700

    Many tweaks to try to explain ASE better (7/1)

--- CisTransASE.py
+++ CisTransASE.py
@@ -22,7 +22,9 @@ from scipy import interpolate
 from multiprocessing import Pool
 from progressbar import ProgressBar as pbar
 from Utils import pd_kwargs, sel_startswith, get_xs
-
+from matplotlib import cm
+import PlotUtils as pu
+import DistributionDifference as dd
 
 
 def fit_all_splines(expr, pool=None, progress=False):
@@ -32,10 +34,20 @@ def fit_all_splines(expr, pool=None, progress=False):
     if pool is True:
         pool = Pool()
 
-    asyncs = {gene: pool.apply_async(interpolate.interp1d,
-                               (xs[is_good],
-                                expr.ix[gene, is_good],
-                                "cubic"))
+    asyncs = {gene: pool.apply_async(interpolate.UnivariateSpline,
+                               (
+                                   xs[is_good],
+                                   pd.rolling_mean(
+                                       expr.ix[gene, is_good],
+                                       5,
+                                       center=True,
+                                       min_periods=1,
+                                   ),
+                                   None, [None, None],
+                                   #3,
+                                   #5,
+                               )
+                                    )
            for gene in expr.index}
     pb = pbar(maxval=len(asyncs) + 1)
 
@@ -48,46 +60,140 @@ def fit_all_splines(expr, pool=None, progress=False):
     return out
 
 
-EXPR_MIN = 5
+EXPR_MIN = 10
 if __name__ == "__main__":
     print("Reading data")
     expr = (pd.read_table('analysis_godot/summary_fb.tsv', **pd_kwargs)
            .dropna(how='all', axis=0)
-           .dropna(how='all', axis=1))
+           )
     ase = (pd.read_table('analysis_godot/ase_summary_by_read.tsv', **pd_kwargs)
+           .select(**sel_startswith(('melXsim', 'simXmel')))
            .dropna(how='all', axis=0)
-           .dropna(how='all', axis=1))
+          )
     paris = pd.read_table('prereqs/GSE68062_Gene_CLASS_after_FPKM_normalization.txt',
                   index_col=1)['mel.CLASS']
+    pzyg = paris[paris != 'mat']
     mel = expr.select(**sel_startswith('mel_'))
     sim = expr.select(**sel_startswith('sim_'))
+    hyb = expr.select(**sel_startswith(('melXsim', 'simXmel')))
 
     both_expr = (mel.max(axis=1) > EXPR_MIN) & (sim.max(axis=1) > EXPR_MIN)
-    both_expr = both_expr.select(ase.index.__contains__)
+    both_expr = (both_expr
+                 .select(ase.index.__contains__)
+                 .select(pzyg.index.__contains__)
+                )
     both_expr = both_expr.index[both_expr]
     mel = mel.ix[both_expr]
     sim = sim.ix[both_expr]
-    print("Fitting splines...")
 
-    with Pool() as p:
-        mel_splines = fit_all_splines(mel, p)
-        sim_splines = fit_all_splines(sim, p)
+    if 'mel_splines' not in locals() or locals().get('recalc', True):
+        print("Fitting splines...")
+        with Pool() as p:
+            mel_splines = fit_all_splines(mel, p)
+            sim_splines = fit_all_splines(sim, p)
+        recalc = False
 
     ase_xs = get_xs(ase)
+    ase_maternals = pd.Series(
+        index=ase_xs.index,
+        data=[1 if col.startswith('simXmel') else -1 for col in ase_xs.index]
+    )
     ase_avgs = pd.DataFrame(
-        data=dict(exprclass='?', actual=np.nan, predicted=np.nan),
+        data=dict(emd=np.nan, exprclass='?', avg_actual=np.nan,
+                  avg_predicted=np.nan, bias=np.nan),
         index=mel.index,
     )
 
-    for gene in pbar()(ase_avgs.index):
-        xg = ase_xs[np.isfinite(ase.ix[gene])]
+    prog = pbar(maxval=len(ase_avgs.index))
+    #prog = lambda x: x
+    for gene in prog(ase_avgs.index):
+        good_ase = np.isfinite(ase.ix[gene]) & ~(ase.ix[gene] == ase_maternals)
+        xg = ase_xs[good_ase]
         ase_avgs.ix[gene, 'actual'] = ase.ix[gene].mean()
-        sim_pred = sim_splines[gene](xg)
-        mel_pred = mel_splines[gene](xg)
-        ase_avgs.ix[gene, 'predicted'] = np.average(
-            ((sim_pred - mel_pred) /(sim_pred + mel_pred)),
-            weights=(sim_pred + mel_pred),
+        sim_pred = pd.Series(sim_splines[gene](ase_xs).clip(0.1, 1e10),
+                             name='predicted_sim_'+gene,
+                             index=ase_xs.index)
+        mel_pred = pd.Series(mel_splines[gene](ase_xs).clip(0.1, 1e10),
+                             name='predicted_mel_'+gene,
+                             index=ase_xs.index)
+        if sum(sim_pred[xg] + mel_pred[xg]) == 0:
+            continue
+        #print(gene)
+        #print("Sim", min(sim_pred), max(sim_pred))
+        #print("Mel", min(mel_pred), max(mel_pred))
+        pred_ase = (
+            ((sim_pred - mel_pred) /(sim_pred + mel_pred))
         )
+        pred_ase.name='predicted_ase'
+        '''
+        print(pd.DataFrame([
+            pd.Series(sim_pred, index=xg.index),
+            pd.Series(mel_pred, index=xg.index),
+            pred_ase,
+            ase.ix[gene, good_ase],
+        ],
+            index=['sim_pred', 'mel_pred', 'ase_pred', 'ase_actual']
+        ).T.to_csv(sep='\t', float_format='%.02f'))
+        '''
+
+        ase_avgs.ix[gene, 'predicted'] = np.average(
+            pred_ase[good_ase],
+            weights=(sim_pred + mel_pred)[good_ase],
+        )
+        ase_avgs.ix[gene, 'bias'] = (
+            (ase.ix[gene, good_ase] - pred_ase[good_ase])
+        ).mean()
+
+        pu_kwargs = {
+            'box_height': 60,
+             'col_sep': '_sl',
+             'convert': True,
+             'draw_box': True,
+             'draw_name': False,
+             'draw_row_labels': True,
+             'make_hyperlinks': True,
+             'max_width': 1320,
+             'progress_bar': False,
+             'split_columns': True,
+             'total_width': 200,
+             'vspacer': 0}
+        pu.svg_heatmap(
+            (
+                None, mel.ix[gene], None, None, None, None,
+                None, mel_pred,
+                None, sim.ix[gene], None, None, None, None,
+                None, sim_pred,
+                None, ase.ix[gene],
+                None, pred_ase,
+                None, hyb.ix[gene],
+            ),
+            'analysis_godot/results/ase_preds/{}.svg'.format(gene),
+            norm_rows_by=('mel expression', 'max', '', '', '', '',
+                          'predicted mel expression', 'max',
+                          'sim expression', 'max', '', '', '', '',
+                          'predicted sim expression', 'max',
+                          'ASE', 'center0pre',
+                          'predicted ASE', 'center0pre',
+                          'hybrid expression', 'max',
+                         ),
+            cmap=(None, pu.ISH, None, None, None, None,
+                  None, cm.Reds,
+                  None, pu.ISH, None, None, None, None,
+                  None, cm.Blues,
+                  None, cm.RdBu,
+                  None, cm.RdBu,
+                  None, pu.ISH,
+                 ),
+            **pu_kwargs
+        )
+        '''
+        if len(xg):
+            ase_avgs.ix[gene, 'emd'] = dd.earth_mover_multi(
+                ase.ix[gene, good_ase],
+                pred_ase,
+                #normer=lambda x: 1,
+            )
+            '''
         if gene in paris.index:
             ase_avgs.ix[gene, 'exprclass'] = paris[gene]
 
