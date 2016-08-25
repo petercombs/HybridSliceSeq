c57ac3975dd3f0fda5d3d6a9410297baa5b15873
petercombs <pcombs@stanford.edu>
2016-08-25 11:36:58 -0700

    Ignore exonic peaks

--- Get_DNase_SNP_overlaps.sh
+++ Get_DNase_SNP_overlaps.sh
@@ -12,19 +12,35 @@ bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/DNase_R6.
 
 # Zelda Peaks
 bedtools window -w 5000 -b analysis/results/no_svase_clean.bed -a Reference/li13_r6.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
 	> analysis/results/no_svase_zld.bed
 bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/li13_r6.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
 	> analysis/results/has_svase_zld.bed
 
 # ORegAnno features
-bedtools window -w 5000 -b analysis/results/no_svase_clean.bed -a Reference/oreganno_tfbs.bed -u > analysis/results/no_svase_tfbs.bed
-bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/oreganno_tfbs.bed -u > analysis/results/has_svase_tfbs.bed
-bedtools window -w 5000 -b analysis/results/no_svase_clean.bed -a Reference/oreganno_reg.bed -u > analysis/results/no_svase_reg.bed
-bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/oreganno_reg.bed -u > analysis/results/has_svase_reg.bed
+bedtools window -w 5000 -b analysis/results/no_svase_clean.bed -a Reference/oreganno_tfbs.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	> analysis/results/no_svase_tfbs.bed
+bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/oreganno_tfbs.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	> analysis/results/has_svase_tfbs.bed
+bedtools window -w 5000 -b analysis/results/no_svase_clean.bed -a Reference/oreganno_reg.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	> analysis/results/no_svase_reg.bed
+bedtools window -w 5000 -b analysis/results/has_svase.bed -a Reference/oreganno_reg.bed -u \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	> analysis/results/has_svase_reg.bed
 
 
 # Remove DNase peaks that are near genes with and without svASE
-bedtools intersect -a analysis/results/no_svase_dnase.bed -b analysis/results/has_svase_dnase.bed -v >  analysis/results/no_svase_dnase_clean.bed
+bedtools intersect -a analysis/results/no_svase_dnase.bed -b analysis/results/has_svase_dnase.bed -v \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	>  analysis/results/no_svase_dnase_clean.bed
+bedtools intersect -a analysis/results/no_svase_zld.bed -b analysis/results/has_svase_zld.bed -v \
+	| bedtools subtract -a - -b Reference/exons.bed \
+	>  analysis/results/no_svase_zld_clean.bed
+
 
 # Count number of SNPs overlapping each DNase peak
 bedtools intersect -a analysis/results/has_svase_dnase.bed -b analysis/on_mel/melsim_variant_noprepend.bed -c > analysis/results/has_svase_dnase_snps.bed
