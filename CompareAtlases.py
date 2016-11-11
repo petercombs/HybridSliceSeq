1592be7d1142d990b67facef58cf86fac190c41c
petercombs <pcombs@stanford.edu>
2016-11-10 16:28:21 -0800

    Make some effort to match the mel and sim histograms
    Ensure roughly the same variance, and set "0" to be the average value of
    points in the non-expressed region between the stripes.

--- CompareAtlases.py
+++ CompareAtlases.py
@@ -27,6 +27,11 @@ if __name__ == "__main__":
     mel_selector = (mel_atlas_expr[:, 'hb', -2] > .3) & (mel_atlas_pos.ix[:, 'pctX', -2] > 75)
     sim_selector = (sim_atlas_expr[:, 'hb', -2] > .3) & (sim_atlas_pos.ix[:, 'pctX', -2] > 75)
 
+    mel_background = ((50 < mel_atlas_pos.ix[:, 'pctX', -2])
+                      & (mel_atlas_pos.ix[:, 'pctX', -2] < 75))
+    sim_background = ((50 < sim_atlas_pos.ix[:, 'pctX', -2])
+                      & (sim_atlas_pos.ix[:, 'pctX', -2] < 75))
+
     print(
         mel_atlas_expr.ix[mel_selector, 'hb', -2].mean(),
         sim_atlas_expr.ix[sim_selector, 'hb', -2].mean(),
@@ -37,15 +42,32 @@ if __name__ == "__main__":
     scatter(mel_atlas_pos[:, 'X', 4], mel_atlas_pos[:, 'Z', 4], c=mel_selector, s=80)
     scatter(sim_atlas_pos[:, 'X', 4], sim_atlas_pos[:, 'Z', 4], c=sim_selector, s=80)
 
+    target = 'hb'
     factor = 3
     mel_grid_expr = zeros((101//factor+1, 101//factor+1))
     mel_grid_ns = zeros_like(mel_grid_expr)
     sim_grid_expr = zeros((101//factor+1, 101//factor+1))
     sim_grid_ns = zeros_like(sim_grid_expr)
 
+    mel_expr_at_stage = (
+        mel_atlas_expr.ix[:, target, -2]
+        - np.nanmean(mel_atlas_expr.ix[mel_background, target, -2])
+    )#.clip(0, np.inf)
+    mel_expr_at_stage /= np.nanpercentile(
+        mel_atlas_expr.ix[~mel_background, target, -2],
+        90
+    )
+    sim_expr_at_stage = (
+        sim_atlas_expr.ix[:, target, -2]
+        - np.nanmean(sim_atlas_expr.ix[sim_background, target, -2])
+    )#.clip(0, np.inf)
+    sim_expr_at_stage /= np.nanpercentile(
+        sim_atlas_expr.ix[~sim_background, target, -2],
+        90
+    )
     for x, z, expr in zip(mel_atlas_pos.ix[:, 'pctX', -2],
                           mel_atlas_pos.ix[:, 'pctZ', -2],
-                          mel_atlas_expr.ix[:, 'hb', -2]):
+                          mel_expr_at_stage):
         x = int(x)//factor
         z = int(z)//factor
         mel_grid_expr[z, x] += expr
@@ -53,7 +75,7 @@ if __name__ == "__main__":
 
     for x, z, expr in zip(sim_atlas_pos.ix[:, 'pctX', -2],
                           sim_atlas_pos.ix[:, 'pctZ', -2],
-                          sim_atlas_expr.ix[:, 'hb', -2]):
+                          sim_expr_at_stage):
         x = int(x)//factor
         z = int(z)//factor
         sim_grid_expr[z, x] += expr
@@ -63,8 +85,12 @@ if __name__ == "__main__":
     sim_grid_avg = sim_grid_expr / sim_grid_ns
 
 
-    mel_grid_avg /= nanpercentile(mel_grid_avg, 90)
-    sim_grid_avg /= nanpercentile(sim_grid_avg, 90)
+    mel_grid_lo = nanpercentile(mel_grid_avg, 10)
+    mel_grid_hi = nanpercentile(mel_grid_avg, 90)
+    sim_grid_lo = nanpercentile(sim_grid_avg, 10)
+    sim_grid_hi = nanpercentile(sim_grid_avg, 90)
+    mel_grid_avg = (mel_grid_avg - mel_grid_lo) / (mel_grid_hi - mel_grid_lo)
+    sim_grid_avg = (sim_grid_avg - sim_grid_lo) / (sim_grid_hi - sim_grid_lo)
 
     ax = gca()
     ax.set_aspect(1)
@@ -73,7 +99,7 @@ if __name__ == "__main__":
 
 
     figure()
-    heatmap = pcolormesh((sim_grid_avg - mel_grid_avg) / np.where(denom > 1,
+    heatmap = pcolormesh((sim_grid_avg - mel_grid_avg) / np.where(denom > .5,
                                                                   denom,  np.nan),
                vmin=-1, vmax=1, cmap=cm.RdBu)
     heatmap.cmap.set_bad((.5, .5, .5))
