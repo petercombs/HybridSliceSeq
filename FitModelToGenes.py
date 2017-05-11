c6527eb4d770b892ff316c542ef8bbf6bdb97176
petercombs <pcombs@stanford.edu>
2017-05-11 12:14:37 -0700

    Tons of plotting additions, including virtual sliced ase
    Also reading in arguments from the command line. Finally.

--- FitModelToGenes.py
+++ FitModelToGenes.py
@@ -1,22 +1,27 @@
 from __future__ import print_function
 import PointClouds as pc
-from sklearn.linear_model import logistic
+#from sklearn.linear_model import logistic
 from statsmodels.api import Logit
 import PlotUtils as pu
 from matplotlib import pyplot as mpl
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
+from matplotlib.transforms import Bbox
 import numpy as np
 import pandas as pd
 import multiprocessing as mp
 import itertools as it
+import setcolor
 from progressbar import ProgressBar as pbar
-from collections import Counter
+from collections import defaultdict
 from sys import argv
 from os import path
+import Utils as ut
+from argparse import ArgumentParser
 
 
-target_gene = 'hb'
+#target_gene = 'hb'
+target_gene = 'comm2'
 input_genes = ['bcdP', 'kni', 'gtP']
 
 all_regs = set(('brk bun cad CG10924 CG17786 CG4702 cnc croc hkb kni tll Traf1 trn '
@@ -25,16 +30,33 @@ all_regs = set(('brk bun cad CG10924 CG17786 CG4702 cnc croc hkb kni tll Traf1 t
 all_regs = set(('bcdP bcdP2 cad gtP kni KrP hkb hkb2 tll D da dl mad med shn '
                 'sna twi').split())
 all_regs = set(('bcdP bcdP2 cad gtP KrP tll D da dl mad med shn '
-                'sna twi').split())
+                'sna twi zen brk emc numb rho tkv Doc2').split())
 
 
-time_point = 'T2'
+times = {
+    'Ance': 'T4',
+    'hb': 'T3',
+    'path': 'T3',
+    'Bsg25A': 'T4',
+    'pxb': 'T4',
+    'bmm': 'T4',
+    'comm2': 'T4',
+    'slp1': 'T2',
+    'CG8147': 'T4',
+}
 
 all_changes = {
+    'Ance': {
+        'gtP': 0.5, 'Kr': 2.0, 'zen': 2.0,
+        'Doc2': 0.5, 'twi': 2.0, 'kni': 0.5,
+        'tll': 2.0, 'hkb': 0.5, 'D': 0.5, 'sna': 2.0,
+    },
     'hb': {
-        'hkb' : 0.5, 'hkb2': 0.5, 'kni': 0.5, 'D': 1.0, 'KrP': 0.5,
-        'gtP': 0.5, 'tll': 1.0, 'bcdP2': 2.0, 'bcdP': 2,
-        'sna' : 2.0,
+        'D': 2.0, 'twi': 2.0,
+        #'gtP': 0.5, 'tll': 1.0,
+        'bcdP2': 2.0, 'bcdP': 2,
+        #'hkb' : 0.5, 'hkb2': 0.5, 'kni': 0.5, 'D': 1.0, 'KrP': 0.5,
+        #'sna' : 2.0,
     },
     'Kr': {
         'gtP': 0.5, 'tll': 2.0, 'hkb': 0.5, 'kni': 2.0, 'D': 0.5,
@@ -44,11 +66,27 @@ all_changes = {
     'Bsg25A': {
         'gtP': 2.0, 'hkb': 2.0, 'KrP': 2.0, 'hbP': 0.5,
 
-    }
+    },
+    'path': {
+        'twi': 0.5, 'hb': 0.5, 'kni': 2.0,
+        'hkb': 2.0, 'cad': 0.5, 'tll': 2.0,
+
+    },
+    'comm2': {
+        'hkb': 2.0, 'KrP': 0.5, 'gtP': 2.0,
+        'D': 0.5,
+
+    },
+    'bmm':{
+
+    },
+    'CG8147':{
+        'twi': 0.5, 'hkb': 2.0, 'Kr':2.0, 'D': 2.0,
+        'tll': 0.5,
+    },
 }
 
 
-changes = all_changes[target_gene]
 
 def plot_changes(model, changes, atlas, new_figure=True, xlims=None, ylims=None,
                 hide_low_expr=False):
@@ -82,11 +120,127 @@ def plot_changes(model, changes, atlas, new_figure=True, xlims=None, ylims=None,
             edgecolors=(0, 0, 0, 0),
         )
 
-    mpl.ylabel('predicted ASE data')
+    #mpl.ylabel('predicted ASE data')
     mpl.title(str(changes))
     ax = mpl.gca()
     ax.set_aspect(1)
     ax.set_axis_bgcolor((.6, .6, .6))
+
+def get_virtual_ase_slices(model, changes, atlas, n_slices, fitter=None,
+                           denom_cutoff=5):
+    ''' Calculate predicted ASE with model and single TF changes
+
+    To use a fixed value for expression levels in the denominator, use a
+    positive value for denom_cutoff.
+
+    To have a fixed number of slices with non-nan expression, use a negative
+    value for denom_cutoff.
+
+    '''
+    X = atlas.ix[:, model.params.index]
+    p1 = model.predict(X)
+    if fitter is None:
+        fitter = Predictor(model, changes)
+    p2 = fitter.predict(X)
+    virtualslices = np.empty((1, n_slices)) * np.nan
+    denoms = np.zeros(n_slices)
+    pct_x = atlas.x - atlas.x.min()
+    pct_x *= 100 /pct_x.max()
+    for i in range(n_slices):
+        in_slice = np.array(((100.0 * i / n_slices < pct_x)
+                    & (pct_x < 100.0 * (i+1) / n_slices)))
+        mel_in_slice = p1[in_slice].clip(0, 1).sum()
+        sim_in_slice = p2[in_slice].clip(0, 1).sum()
+        denom_i = mel_in_slice + sim_in_slice
+        if denom_i > denom_cutoff:
+            virtualslices[0, i] = (sim_in_slice - mel_in_slice) / denom_i
+        else:
+            virtualslices[0, i] = np.nan
+        denoms[i] = denom_i
+    if denom_cutoff < 0:
+        virtualslices[0, denoms < sorted(denoms)[denom_cutoff]] = np.nan
+    return denoms, virtualslices, fitter
+
+
+def compare_ase(ase, model, changes, atlas, denom_cutoff=5):
+    adaptive = denom_cutoff == 'adaptive'
+    embryos = {col.split('sl')[0] for col in ase.index}
+    pred_ase = pd.Series(index=ase.index, data=np.nan)
+    pred_expr = pd.Series(index=ase.index, data=np.nan)
+    fitter = None
+    for embryo in embryos:
+        emb_ase = ase.select(ut.startswith(embryo))
+        if adaptive:
+            denom_cutoff = -emb_ase.count()
+        (denoms, virtualase, fitter) = get_virtual_ase_slices(
+            model, changes, atlas, len(emb_ase),
+            fitter=fitter, denom_cutoff=denom_cutoff,
+        )
+        pred_ase.ix[emb_ase.index] = virtualase[0]
+        pred_expr.ix[emb_ase.index] = denoms
+
+    return pred_ase.corr(ase), pred_ase, pred_expr
+
+def calculate_r2(ase, prediction, mean_norm = False):
+    if mean_norm:
+        ase = ase - ase.mean()
+        prediction = prediction - prediction.mean()
+    return 1-(
+                    ((ase - prediction)**2).sum()
+                    / ((ase - ase.mean())**2).sum()
+                )
+
+def compare_ase_range(ase, model, tfs, atlas, range_low=0.1, range_high=10,
+                      num_points=51, spacing='log',
+                      denom_cutoff=5):
+    if spacing == 'log':
+        values = np.exp(np.linspace(np.log(range_low), np.log(range_high), num_points,
+                        endpoint=True))
+    elif spacing == 'linear':
+        values = np.linspace(range_low, range_high, num_points, endpoint=True)
+    else:
+        raise ValueError("argument 'spacing' should be one of {'log', 'linear'}")
+
+    if not (isinstance(tfs, tuple) or isinstance(tfs, list)):
+        tfs = [tfs]
+
+
+    corrs = np.zeros_like(values)
+    r2s = np.zeros_like(values)
+    for i, value in enumerate(values):
+        corr, pred_ase, pred_expr = compare_ase(ase, model, {tf: value for tf in tfs},
+                                 atlas, denom_cutoff)
+        corrs[i] = corr
+        r2s[i] = calculate_r2(ase, pred_ase, mean_norm=True)
+    return values, corrs, r2s
+
+
+
+
+def plot_virtual_ase_slices(model, changes, atlas, new_figure=True, n_slices=25,
+                       fitter=None, denom_cutoff=5):
+
+    denoms, virtualslices, fitter = get_virtual_ase_slices(
+        model, changes, atlas, n_slices,
+        fitter=fitter, denom_cutoff=denom_cutoff
+    )
+    if new_figure:
+        mpl.figure()
+    virtualslices = np.ma.masked_where(np.isnan(virtualslices), virtualslices)
+    mpl.cm.RdBu.set_bad((0.5, 0.5, 0.5))
+    mpl.cm.RdBu.set_over((0.5, 0.5, 0.5))
+    mpl.cm.RdBu.set_under((0.5, 0.5, 0.5))
+    mpl.pcolormesh(virtualslices, cmap=mpl.cm.RdBu, vmin=-1, vmax=1)
+    ax = mpl.gca()
+    #ax.set_aspect(1)
+    ax.hlines([0, 1], 0, n_slices)
+    ax.vlines([0, n_slices], 0, 1)
+    ax.set_xlim(-n_slices * .1, n_slices * 1.1)
+    ax.set_ylim(-0.1, 1.1)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    return denoms, virtualslices
+
 
 class Predictor():
     def __init__(self, model, changes):
@@ -106,13 +260,32 @@ def fit_model(X, y, co=0.1):
     sm = Logit((y.clip(0, 1)>co).astype(float), X.clip(0, 1), missing='drop')
     return sm.fit(disp=False)
 
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--atlas',
+                        default='/home/pcombs/HybridSliceSeq/prereqs/D_mel_wt__atlas_r2.vpc')
+    parser.add_argument('--dir', default='/home/pcombs/HybridSliceSeq/')
+    parser.add_argument('--find-best-tweaks', default=False,
+                        action='store_true')
+    parser.add_argument('target_gene', default='hb')
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
+    target_gene = args.target_gene
+    time_point = times[target_gene]
+    changes = all_changes[target_gene]
+    print('-'*32, target_gene, '-'*32, sep='\n')
+    dir = args.dir
     if 'pcr' not in locals():
-        dir = path.dirname(argv[0])
         pcr = pc.PointCloudReader(open(
-            path.join(dir, 'prereqs/D_mel_wt__atlas_r2.vpc')
+            args.atlas
         ))
         atlas_expr, atlas_coords = pcr.data_to_arrays()
+        atlas_expr.ix[:, 'const', :] = 1
+        atlas_expr.ix[:, 'bcdP2', :] = atlas_expr.ix[:, 'bcdP', :]**2
+        atlas_expr.ix[:, 'hkb2', :] = atlas_expr.ix[:, 'hkb', :]**2
 
     for gene in atlas_expr.major_axis:
         last_t = None
@@ -123,9 +296,6 @@ if __name__ == "__main__":
                 last_t = t
 
 
-    atlas_expr.ix[:, 'const', :] = 1
-    atlas_expr.ix[:, 'bcdP2', :] = atlas_expr.ix[:, 'bcdP', :]**2
-    atlas_expr.ix[:, 'hkb2', :] = atlas_expr.ix[:, 'hkb', :]**2
     X = atlas_expr.ix[:, input_genes, time_point].T
     #X['bcd2'] = X['bcdP']**2
     X['const'] = 1
@@ -180,11 +350,14 @@ if __name__ == "__main__":
             'c' : atlas_expr.ix[:, target_gene, time_point],
     })
 
-    polys = {
+    polys = defaultdict(lambda : [all_poly])
+    polys.update({
         'hb': [poly12],
         'Kr': [kr_poly],
         'Bsg25A': [all_poly],
-    }
+        'Ance': [all_poly],
+    })
+
 
     for poly in polys[target_gene]:
 
@@ -204,21 +377,24 @@ if __name__ == "__main__":
         sm_fit = sm_fitter.fit()
 
         Y_tmp = atlas_expr.ix[in_central, target_gene,time_point].copy()
+        Y_tmp /= Y_tmp.max()
+        Y_tmp = 1.0 * (Y_tmp > .5)
 
         all_regs = atlas_expr.ix[:, all_regs, time_point].count(axis=1) > 0
         all_regs = all_regs.index[all_regs]
 
 
-        if True:
+        #if True:
         #if (poly == poly1) or (poly == poly2) or (poly == poly12):
-        #if False:
+        if target_gene == 'hb':
             #best_tfs = ['bcdP', 'hkb', 'hkb2', 'KrP', 'bcdP2', 'const']
             #best_tfs = ['bcdP', 'bcdP2', 'gtP', 'kni', 'hkb',  'KrP', 'const']
             #best_tfs = atlas_expr.major_axis
             best_tfs = {'bcdP', 'bcdP2', 'cad', 'gtP', 'hbP', 'KrP', 'hkb',
-                        'tll', 'D', # 'kni',
+                        'tll', 'D',  'kni',
                         'sna',
                         'h', 'dl', 'mad', 'shn', 'twi', 'const'}
+            best_tfs = {'const', 'bcdP', 'bcdP2', 'D', 'twi'}
             best_tfs.discard(target_gene)
             best_tfs.discard(target_gene + 'P')
             best_model = fit_model(atlas_expr.ix[in_central, best_tfs,
@@ -229,8 +405,15 @@ if __name__ == "__main__":
             with mp.Pool(1) as p:
                 pool = {}
                 for comb in it.combinations(all_regs, 4):
-                    comb = comb + ('hkb', 'kni', 'const', )
-                    X_tmp = atlas_expr.ix[in_central, comb, time_point].T.copy()
+                    comb = tuple(set(comb) |
+                                 set(all_changes[target_gene].keys()))
+                    comb = comb + ('const', )
+                    X_tmp = (atlas_expr.ix[in_central, comb, time_point]
+                             .T.copy()
+                             .dropna(how='all', axis=1)
+                            )
+                    comb = tuple(X_tmp.columns)
+                    if comb in pool: continue
                     pool[comb] = p.apply_async(fit_model, (X_tmp, Y_tmp, co))
                 outs = {}
                 pr2 = pd.Series(index=pool.keys(), data=np.nan)
@@ -263,14 +446,18 @@ if __name__ == "__main__":
             edgecolors=(0, 0, 0, 0),
             cmap=pu.ISH,
         )
+        ax.set_axis_bgcolor((.6, .6, .6))
         mpl.ylabel('Original data')
         print(len(small_atlas.c))
         ax.add_patch(PathPatch(poly, fill=False))
-        xlims = mpl.xlim()
-        xlims = (xlims[0] - 10, xlims[1] + 10)
+        #xlims = mpl.xlim()
+        #xlims = (xlims[0] - 10, xlims[1] + 10)
+        emb_width = small_atlas.x.max() - small_atlas.x.min()
+        xlims = (small_atlas.x.min() - .1 * emb_width,
+                 small_atlas.x.max() + .1 * emb_width)
         mpl.xlim(*xlims)
         ylims = mpl.ylim()
-        ylims = (ylims[0] - 20, ylims[1] + 20)
+        ylims = (ylims[0] - 40, ylims[1] + 40)
         mpl.ylim(*ylims)
         mpl.yticks([])
 
@@ -289,7 +476,7 @@ if __name__ == "__main__":
         mpl.ylabel('Fitting')
         '''
 
-        mpl.subplot(3, 2, 3, aspect='equal')
+        ax = mpl.subplot(3, 2, 3, aspect='equal')
 
         p1 = best_model.predict(best_X.ix[in_central])
         mpl.scatter(
@@ -299,13 +486,16 @@ if __name__ == "__main__":
             c=p1,
             edgecolors=(0,0,0,0),
             cmap=pu.ISH,
+            vmax=1.2,
         )
+        ax.set_axis_bgcolor((.6, .6, .6))
         #print(fitter.score(X.ix[in_central, :], y.ix[in_central] > co))
         mpl.xlim(*xlims)
+        mpl.ylim(*ylims)
         mpl.yticks([])
-        mpl.ylabel('Predicted Mel Data')
+        #mpl.ylabel('Predicted Mel Data')
 
-        mpl.subplot(3, 2, 5, aspect='equal')
+        ax = mpl.subplot(3, 2, 5, aspect='equal')
 
         fitter2 = Predictor(best_model, changes)
         p2 = fitter2.predict(best_X.ix[in_central])
@@ -318,11 +508,13 @@ if __name__ == "__main__":
             edgecolors=(0,0,0,0),
             cmap=pu.cm.RdBu,
         )
+        ax.set_axis_bgcolor((.6, .6, .6))
         mpl.xlim(*xlims)
+        mpl.ylim(*ylims)
         mpl.yticks([])
         mpl.ylabel('predicted ASE data')
 
-        mpl.subplot(3, 2, 4, aspect='equal')
+        ax = mpl.subplot(3, 2, 4, aspect='equal')
         mpl.scatter(
             atlas_coords.ix[in_central, 'X', time_point],
             atlas_coords.ix[in_central, 'Z', time_point],
@@ -331,22 +523,103 @@ if __name__ == "__main__":
             edgecolors=(0,0,0,0),
             cmap=pu.ISH,
         )
+        ax.set_axis_bgcolor((.6, .6, .6))
         mpl.xlim(*xlims)
+        mpl.ylim(*ylims)
         mpl.yticks([])
         mpl.ylabel('predicted sim data')
 
     mpl.savefig(path.join(dir,
                           'analysis/results/model_tweak/{}'.format(target_gene)))
+    setcolor.set_backgroundcolor(mpl.gca(), 'k')
+    setcolor.set_foregroundcolor(mpl.gca(), 'w')
+    mpl.savefig(path.join(dir,
+                          'analysis/results/model_tweak/{}_bgblk'.format(target_gene)))
     print(best_model.summary().as_latex(),
           file=open(path.join(dir,
                               'analysis/results/model_tweak/{}_model.tex'.format(target_gene)),
                     'w')
          )
+    ase = (
+        pd.read_table('analysis_godot/ase_summary_by_read.tsv', **ut.pd_kwargs)
+        .ix[target_gene]
+        .select(ut.startswith(('melXsim', 'simXmel')))
+    )
+    n_good = ase.select(ut.startswith('melXsim_cyc14C_rep1')).count()
     for tf in (best_model
                .params.index.intersection(
                    [key for key, value in changes.items() if value != 1])
               ):
+        mpl.cm.RdBu.set_bad((0.5, 0.5, 0.5))
+        mpl.cm.RdBu_r.set_bad((0.5, 0.5, 0.5))
+        fig = mpl.figure()
+        ax1 = mpl.subplot2grid((3,1), (0,0), 2, 1)
         plot_changes(best_model, {tf: changes[tf]}, small_atlas.ix[small_atlas.in_central], xlims=xlims,
-                     ylims=ylims)
+                     ylims=ylims, new_figure=False)
+        ax2 = mpl.subplot2grid((3,1), (2,0), 2, 1)
+        plot_virtual_ase_slices(best_model, {tf: changes[tf]}, small_atlas.ix[small_atlas.in_central],
+                     new_figure=False, n_slices=27, denom_cutoff=-n_good)
+        ax2.set_frame_on(False)
+        mpl.tight_layout()
+        mpl.ioff()
+        mpl.draw()
+        mpl.show()
+        mpl.ion()
+        mpl.draw_if_interactive()
+        mpl.show()
         mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_{}'
                     .format(target_gene, tf)))
+        bbox1_pts = ax1.get_position().get_points()
+        bbox2_pts = ax2.get_position().get_points()
+        bbox2_pts[:, 0] = bbox1_pts[:, 0]
+        ax1.set_position(Bbox(bbox1_pts))
+        ax2.set_position(Bbox(bbox2_pts))
+
+
+        #mpl.tight_layout()
+        mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_{}'
+                    .format(target_gene, tf)))
+        setcolor.set_backgroundcolor(ax1, 'k')
+        setcolor.set_foregroundcolor(ax1, 'w')
+        mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_{}_bgblk'
+                    .format(target_gene, tf)))
+        print(
+            {tf: changes[tf]},
+            compare_ase(ase, best_model, {tf: changes[tf]},
+                        small_atlas.ix[small_atlas.in_central],
+                        denom_cutoff='adaptive'
+                       )[0]
+             )
+
+
+
+    if 'bcdP' in changes and 'bcdP2' in changes:
+        fig = mpl.figure()
+        ax1 = mpl.subplot2grid((3,1), (0,0), 2, 1)
+        plot_changes(best_model, {'bcdP': 2.0, 'bcdP2': 2.0}, small_atlas.ix[small_atlas.in_central], xlims=xlims,
+                     ylims=ylims, new_figure=False)
+        ax2 = mpl.subplot2grid((3,1), (2,0), 2, 1)
+        plot_virtual_ase_slices(best_model, {'bcdP': 1.5, 'bcdP2': 2.0}, small_atlas.ix[small_atlas.in_central],
+                     new_figure=False, n_slices=27, denom_cutoff=-n_good)
+        #mpl.tight_layout()
+        mpl.tight_layout()
+        mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_bcdbcd2'
+                    .format(target_gene)))
+        mpl.ioff()
+        mpl.draw()
+        mpl.show()
+        mpl.ion()
+        ax2.set_frame_on(False)
+        bbox1_pts = ax1.get_position().get_points()
+        bbox2_pts = ax2.get_position().get_points()
+        bbox2_pts[:, 0] = bbox1_pts[:, 0]
+        bbox1_pts[:, 0] = bbox2_pts[:, 0]
+        ax1.set_position(Bbox(bbox1_pts))
+        ax2.set_position(Bbox(bbox2_pts))
+        print(ax1.get_position())
+        mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_bcdbcd2'
+                    .format(target_gene)))
+        setcolor.set_backgroundcolor(ax1, 'k')
+        setcolor.set_foregroundcolor(ax1, 'w')
+        mpl.savefig(path.join(dir, 'analysis/results/model_tweak/{}_bcdbcd2_bgblk'
+                    .format(target_gene)))
